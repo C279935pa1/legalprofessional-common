2102215fd0417224876fb8cf3920740e6c713324
C279935pa1 <BatchuPavan.koushik@thomsonreuters.com>
2021-06-28 18:52:42 +0530

    Initial Commit- Added Entity Folder with Legal Professional Entity class.

---
+++ src/main/java/ner/legalprofessional/entity/LegalProfessional.java
@@ -0,0 +1,9 @@
+package ner.legalprofessional.entity;
+import java.lang.*;
+import java.util.*;
+
+
+
+public class LegalProfessional {
+    System.out.print("In Legal Professional Entity File");
+}
